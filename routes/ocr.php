49d7bee3af49395a909710ce894eba496377b37e
Prem Kalyan <premk27@gmail.com>
2026-06-22 09:42:54 +0530

    Add manual input workflow routes

--- routes/ocr.php
+++ routes/ocr.php
@@ -11,4 +11,26 @@ Route::middleware([
 ])->group(function () {
     Route::get('analyzepdf/manual-input', [ManualInputController::class, 'index'])
         ->name('analyze.pdf.manual-input');
+
+    Route::get('analyzepdf/manual-input/queue', [ManualInputController::class, 'queue'])
+        ->name('analyze.pdf.manual-input.queue');
+
+    Route::get('analyzepdf/manual-input/client-lookup', [ManualInputController::class, 'clientLookup'])
+        ->name('analyze.pdf.manual-input.client-lookup');
+
+    Route::get('analyzepdf/manual-input/{id}', [ManualInputController::class, 'show'])
+        ->whereNumber('id')
+        ->name('analyze.pdf.manual-input.show');
+
+    Route::post('analyzepdf/manual-input/{id}/save', [ManualInputController::class, 'save'])
+        ->whereNumber('id')
+        ->name('analyze.pdf.manual-input.save');
+
+    Route::post('analyzepdf/manual-input/{id}/force-submit', [ManualInputController::class, 'forceSubmit'])
+        ->whereNumber('id')
+        ->name('analyze.pdf.manual-input.force-submit');
+
+    Route::delete('analyzepdf/manual-input/{id}', [ManualInputController::class, 'destroy'])
+        ->whereNumber('id')
+        ->name('analyze.pdf.manual-input.delete');
 });
